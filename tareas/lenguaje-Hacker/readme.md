f2ff49013c456f097726682d5802c1ce80baafd4
Ivan Altamirano <pedrotrill@gmail.com>
2023-07-05 07:46:32 -0300

    otro ejerc

---
+++ tareas/lenguaje-Hacker/readme.md
@@ -0,0 +1,17 @@
+## Transforma un texto a Lenguaje Hacker  
+S1 escrib0 est0 pued3s ent3nderlo?  
+Tu trabajo es tener un texto en lenguaje humano y transformarlo a lenguaje hacker.
+
+const texto = "Texto en lenguaje humano"  
+//realiza tu proceso  
+console.log(textoEnLenguajeHacker)  
+¿Fácil no? No se si está bien llamarlo fácil pero se que puedes realizarlo por eso te voy a añadir una capa de dificultad.  
+En este website https://www.gamehouse.com/blog/leet-speak-cheat-sheet/ encontrarás una lista de reemplazos para el abcedario en la sección "LEET SPEAK ALPHABET"  
+Tu trabajo será tomar una de las opciones al azar que corresponde a cada letra y formar el texto hacker.  
+Ej:  
+hola = |-|()|_/  
+hola = I+I<>7aye  
+chau = </-/(Lv  
+¿Sumamos un nivel más de difícultad?  
+Esto si puede ser difícil pero si la primera parte te ha salido puedes intentarlo.  
+Realiza el ejercicio a la inversa, desde texto hacker a texto humano  
---
+++ tareas/lenguaje-Hacker/textoHacker.js
@@ -0,0 +1,59 @@
+//node textoEncriptado.js "Escribir para ver si el texto hacker funciona"
+if (!process.argv[2]) {
+    console.log("Papu! No escribiste nada.")
+    process.exit(1)
+}
+
+let texto = process.argv[2];
+
+let letraRandom = (arr) => {
+    let random = 0;
+    let length = arr.length;
+    random = Math.floor((Math.random() * length) + 1);
+    return random;
+}
+
+let arrA = ["4", "@", "/\\"];
+index = letraRandom(arrA) - 1;
+texto = texto.replace(/a/gi, arrA[index])
+
+let arrE = ["3", "€", "[-"];
+index = letraRandom(arrE) - 1;
+texto = texto.replace(/e/gi, arrE[index])
+
+let arrI = ["1", "!", "|"];
+index = letraRandom(arrI) - 1;
+texto = texto.replace(/i/gi, arrI[index]);
+
+let arrO = ["0", "()", "[]"];
+index = letraRandom(arrO) - 1;
+texto = texto.replace(/o/gi, arrO[index]);
+
+let arrU = ["(_)", "|_|", "L|"];
+index = letraRandom(arrU) - 1;
+texto = texto.replace(/u/gi, arrU[index]);
+
+// texto = texto.replace(/b/gi, "I3");
+// texto = texto.replace(/c/gi, "[");
+// texto = texto.replace(/d/gi, "|)");
+// texto = texto.replace(/f/gi, "/=");
+// texto = texto.replace(/g/gi, "6");
+// texto = texto.replace(/h/gi, "}{");
+// texto = texto.replace(/j/gi, "_]");
+// texto = texto.replace(/k/gi, "|<");
+// texto = texto.replace(/l/gi, "|_");
+// texto = texto.replace(/m/gi, "/\/\\");
+// texto = texto.replace(/n/gi, "|\\|");
+// texto = texto.replace(/ñ/gi, "|\\°|");
+// texto = texto.replace(/p/gi, "|°");
+// texto = texto.replace(/q/gi, "()_");
+// texto = texto.replace(/r/gi, "|2");
+// texto = texto.replace(/s/gi, "5");
+// texto = texto.replace(/t/gi, "7");
+// texto = texto.replace(/v/gi, "\\/");
+// texto = texto.replace(/w/gi, "\\/\\/");
+// texto = texto.replace(/x/gi, "><");
+// texto = texto.replace(/y/gi, "`/");
+// texto = texto.replace(/z/gi, "2");
+
+console.log(texto);
