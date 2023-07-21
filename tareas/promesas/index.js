eaf3f93048fa33702fa3f074a0230f09cafed9b8
Ivan Altamirano <pedrotrill@gmail.com>
2023-07-21 02:26:24 -0300

    ejercicio de promesas - maquina adivinando num

---
+++ tareas/promesas/index.js
@@ -0,0 +1,29 @@
+const randomNumber = (from = 1, to = 10) => Math.floor(Math.random() * (to - from + 1)) + from;
+
+const buscarNumeroMasCercano = (randNum, numeros) => {
+    const numeroMasCercano = numeros.reduce((prev, curr) => {
+        return Math.abs(curr - randNum) < Math.abs(prev - randNum) ? curr : prev;
+    });
+    return numeroMasCercano;
+}  
+
+const numeros = process.argv.slice(2).map(num => parseInt(num));
+
+const promesa = new Promise((resolve, reject) => {
+    const n = randomNumber();
+    const index = numeros.indexOf(n);
+    if (index !== -1) {
+        resolve(`Ganaste, el número era ${n} y se encontraba en la posición ${index + 1}.`);
+    } else {
+        const numeroMasCercano = buscarNumeroMasCercano(n, numeros);
+        reject(`Perdiste, el número era ${n} y tu número más cercano fue el ${numeroMasCercano}.`);
+    }
+});
+
+promesa
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+    });
---
+++ tareas/promesas/readme.md
@@ -0,0 +1,14 @@
+# Promesas - tarea
+Realiza un programa con promesas que acepte 3 números enteros, por ejemplo:  
+node index.js 4 5 8  
+
+Tu programa debe elegir un número al azar entre 1 y 10.  
+
+Si el número que eligió tu programa coincide con alguno de los 3 elegidos da el programa finalizado con éxito. Indica cual era el número de coincidencia y en qué posición se encontraba.  
+
+Si el número no es encontrado indica cuál era el número al azar elegido por tu programa y cuál de los 3 números ingresados estuvo más cerca. 
+
+| Ganador :white_check_mark:                | Perdedor :x:                       |
+|:------------------------------|:----------------------------------|
+| node index.js 5 7 9           |      node index.js 5 7 9         |  |
+| Ganaste, el número era 7 y se encontraba en la segunda posición.  | Perdiste, el número era 2 y tu número más cercano fue el 5 |
